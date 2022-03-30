24bca9bb683e9a30cbc9a09466920cead51f01a5
furukawa <danieldgt@gmail.com>
2022-03-30 12:47:26 -0300

    prova sobre recursao

--- src/br/com/ifce/estrutura/recursao/HelloRecursao.java
+++ src/br/com/ifce/estrutura/recursao/HelloRecursao.java
@@ -2,8 +2,72 @@ package br.com.ifce.estrutura.recursao;
 
 public class HelloRecursao {
     public static void main(String[] args) {
-        decremento(4);
-        //System.out.println(fatorial(3));
+        //isMultiplo(6,3);
+        //System.out.println(isMultiplo(122, 2));
+        //System.out.println(isPrimoRecursivo(21, 2));
+        //System.out.println(somatorioRecursivo(6));
+        System.out.println(totalDigitosRecursivo(52323));
+        //System.out.println(mdcRecursivo(100, 24));
+        //numerosPrimosDoIntervalo(10, 20);
+    }
+
+    public static int numerosPrimosDoIntervalo(int numero1, int numero2) {
+        if (numero1 == numero2) {
+            return 1;
+        } else {
+            if (isPrimo(numero1) && numero1 > 3) {
+                System.out.println(numero1);
+            }
+            return numerosPrimosDoIntervalo(numero1 + 1, numero2);
+        }
+    }
+
+    public static boolean isPrimo(int numero) {
+        for (int i = 2; i < numero; i++) {
+            if (isMultiplo(numero, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isMultiplo(int numero1, int numero2) {
+        return numero1 % numero2 == 0;
+    }
+
+
+    public static int mdcRecursivo(int numero1, int numero2) {
+        if (numero2 != 0) {
+            return mdcRecursivo(numero2, numero1 % numero2);
+        } else {
+            return numero1;
+        }
+    }
+
+    public static int totalDigitosRecursivo(int numero) {
+        if (numero == 0) {
+            return 0;
+        } else {
+            return 1 + totalDigitosRecursivo(numero / 10);
+        }
+    }
+
+    public static int somatorioRecursivo(int numero) {
+        if (numero == 1) {
+            return 1;
+        } else {
+            return numero + somatorioRecursivo(numero - 1);
+        }
+    }
+
+    public static int isPrimoRecursivo(int numero, int controle) {
+        if (numero == controle) {
+            return 1;
+        } else if (isMultiplo(numero, controle)) {
+            return 0;
+        } else {
+            return isPrimoRecursivo(numero, controle + 1);
+        }
     }
 
     public static void incrementoDecremento(int numero) {
