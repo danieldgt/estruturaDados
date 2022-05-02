78bac27a0fea14b56ee127951db12ca654671b85
root <root@DESKTOP-CTKI49H.localdomain>
2022-05-02 11:40:38 -0300

    lista de exercicios

--- src/br/com/ifce/estrutura/lista/encadeadadupla/ListaNo.java
+++ src/br/com/ifce/estrutura/lista/encadeadadupla/ListaNo.java
@@ -1,11 +1,13 @@
 package br.com.ifce.estrutura.lista.encadeadadupla;
 
+import static java.lang.String.format;
+
 public class ListaNo {
 	DuploNo primeiro;
 	DuploNo ultimo;
 	private int count = 0;
 
-	public void add(DuploNo duploNo) {
+	public void append(DuploNo duploNo) {
 		if (primeiro == null) {
 			primeiro = duploNo;
 		} else if (ultimo == null) {
@@ -17,6 +19,13 @@ public class ListaNo {
 			duploNo.setAnterior(ultimo);
 			ultimo = duploNo;
 		}
+		count++;
+	}
+
+	public void addFist(DuploNo duploNo) {
+		primeiro.setAnterior(duploNo);
+		duploNo.setProximo(primeiro);
+		primeiro = duploNo;
 		count++;
 	}
 
@@ -67,6 +76,64 @@ public class ListaNo {
 			return false;
 		}
 		return true;
+	}
+
+	public int encontrarPosicaoDescricao(String descricao) throws Exception {
+		return buscarPosicaoDuploNoPorDescricao(primeiro, descricao);
+	}
+
+	public int buscarPosicaoDuploNoPorDescricao(DuploNo duploNo, String busca) throws Exception {
+		if (duploNo == null) {
+			throw new Exception("nao encontrado!");
+		}
+		if (duploNo.getDescricao().toUpperCase().equals(busca.toUpperCase())) {
+			return 1;
+		}
+		
+		return 1 + buscarPosicaoDuploNoPorDescricao(duploNo.getProximo(), busca);
+	}
+
+	public String listToString() {
+		int controle = 0;
+		return getStrings(primeiro, controle);
+	}
+
+	public String getStrings(DuploNo duploNo, int controle) {
+		if (duploNo == null) {
+			return "";
+		}
+
+		return format("Posicao: %d; Descricao: %s; %s", controle, duploNo.getDescricao(), "\n")
+				.concat(getStrings(duploNo.getProximo(), controle + 1));
+	}
+
+	public void addPosition(DuploNo noNovo, int posicao) {
+		if (posicao == 0) {
+			addFist(noNovo);
+		} else if (posicao + 1 == count) {
+			append(noNovo);
+		} else {
+			DuploNo noCorrente = encontrarNoPorPosicao(posicao);
+			noNovo.setAnterior(noCorrente.getAnterior());
+			noNovo.setProximo(noCorrente);
+			noCorrente.getAnterior().setProximo(noNovo);
+			noCorrente.setAnterior(noNovo);
+			count++;
+		}
+	}
+
+	public DuploNo encontrarNoPorPosicao(int posicao) {
+		return encontrarPosicao(this.primeiro, posicao);
+	}
+
+	public DuploNo encontrarPosicao(DuploNo duploNo, int posicao) {
+		if (posicao == 0 && duploNo != null) {
+			return duploNo;
+		} else if (duploNo == null) {
+			throw new IndexOutOfBoundsException("posicao não existe!");
+		}
+
+		return encontrarPosicao(duploNo.getProximo(), posicao - 1);
 	}
 
 	public int getCount() {
--- src/br/com/ifce/estrutura/lista/encadeadadupla/TesteDuploEncadeado.java
+++ src/br/com/ifce/estrutura/lista/encadeadadupla/TesteDuploEncadeado.java
@@ -1,23 +1,34 @@
 package br.com.ifce.estrutura.lista.encadeadadupla;
 
+import java.util.LinkedList;
+
 public class TesteDuploEncadeado {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		ListaNo listaDupla = new ListaNo(null, null);
 
-		listaDupla.add(new DuploNo("Daniel"));
-		listaDupla.add(new DuploNo("Joao"));
-		listaDupla.add(new DuploNo("Francisco"));
-		listaDupla.add(new DuploNo("Raimundo"));
+		listaDupla.append(new DuploNo("Daniel"));
+		listaDupla.append(new DuploNo("Joao"));
+		listaDupla.append(new DuploNo("Francisco"));
+		listaDupla.append(new DuploNo("Raimundo"));
 
 		System.out.println("Count manual: " + listaDupla.countManual());
 		System.out.println("Count por variavel: " + listaDupla.getCount());
-		
+
 		System.out.println("Tem nome francisco? " + listaDupla.containsName("francisco2"));
-	
-		//TODO: dizer qual posicao foi encontrado o nome?
-		//TODO: imprimir a lista com quebra de linha e o numero da posição
-		//TODO: add em uma posição específica um determinado no
-		//TODO: add na frente
+
+		// TODO: dizer qual posicao foi encontrado o nome?
+		System.out.println(listaDupla.encontrarPosicaoDescricao("Joao"));
+		// TODO: imprimir a lista com quebra de linha e o numero da posição
+		System.out.println(listaDupla.listToString());
+		// TODO: add em uma posição específica um determinado nó
+		listaDupla.addPosition(new DuploNo("Pedro"), 3);
+		System.out.println(listaDupla.listToString());
+		// TODO: add na frente
+		listaDupla.addFist(new DuploNo("Maria"));
+		System.out.println(listaDupla.listToString());
+		
+		//BONUS
+		//CRIE UMA FIFO E UMA LIFO
 	}
 }
