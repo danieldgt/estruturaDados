858fe68c08d5f73546800c72365cd28ecbc9fd84
Daniel Alencar Barros Tavares <danieldgt@hotmail.com>
2022-05-09 11:40:33 -0300

    tarefa

---
+++ src/br/com/ifce/estrutura/arvore/HelloArvore.java
@@ -0,0 +1,12 @@
+package br.com.ifce.estrutura.arvore;
+
+public class HelloArvore {
+
+	public static void main(String[] args) {
+		
+		ArvoreBinario arvoreBinario = new ArvoreBinario();
+		arvoreBinario.addNo("folha1", "esquerda", null);
+		arvoreBinario.addNo("folha2", "direita", null);
+		
+	}
+}
